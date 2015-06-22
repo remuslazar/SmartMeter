fb0f2d25134804827ee18b95604c05f95252472a
Remus Lazar <remus@lazar.info>
2015-06-22 16:17:56 +0200

    Axes drawing to a separate class

---
+++ SmartMeter/AxesDrawer.swift
@@ -0,0 +1,83 @@
+//
+//  AxesDrawer.swift
+//  SmartMeter
+//
+//  Created by Remus Lazar on 22.06.15.
+//  Copyright (c) 2015 Remus Lazar. All rights reserved.
+//
+
+import UIKit
+
+class AxesDrawer: UIView {
+
+    private struct Constants {
+        static let LabelHorizontalSpacing: CGFloat = 120
+        static let LabelVerticalSpacing: CGFloat = 60
+        static let XStepValueShouldBeMultipleOf: CGFloat = 60 // seconds
+        static let YStepValueShouldBeMultipleOf: CGFloat = 100
+        static let XScaleMarginBottom: CGFloat = 42
+        static let YScaleMarginLeft: CGFloat = 8
+        static let LegendColor: UIColor = UIColor.lightGrayColor()
+    }
+
+    private func drawText(text: String, toPoint point: CGPoint) {
+        let attributes = [
+            NSFontAttributeName : UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote),
+            NSForegroundColorAttributeName : Constants.LegendColor
+        ]
+
+        var textRect = CGRect(origin: point, size: text.sizeWithAttributes(attributes))
+        text.drawInRect(textRect, withAttributes: attributes)
+    }
+    
+    private func drawY(bounds: CGRect, minY: CGFloat, maxY: CGFloat) {
+        let maximumNumberOfLabels = bounds.height / Constants.LabelVerticalSpacing
+        let minStepValue = maxY / maximumNumberOfLabels
+        let stepValue = minStepValue.multipleOf(Constants.YStepValueShouldBeMultipleOf)
+        
+        let formatter = NSNumberFormatter()
+        formatter.maximumFractionDigits = 1
+        formatter.minimumIntegerDigits = 1
+        
+        for var yValue: CGFloat = stepValue ; yValue < maxY ; yValue += stepValue {
+            let y = bounds.height - (yValue / maxY * bounds.height)
+            if let label = formatter.stringFromNumber(yValue)?.stringByAppendingString("W") {
+                drawText(label, toPoint: CGPoint(x: Constants.YScaleMarginLeft, y: y))
+            }
+        }
+    }
+    
+    private func drawX(bounds: CGRect, minX: NSDate, maxX: NSDate) {
+        let timeInterval = maxX.timeIntervalSinceDate(minX)
+        let maximumNumberOfLabels = bounds.width / Constants.LabelHorizontalSpacing
+        let minStepValue = CGFloat(timeInterval) / maximumNumberOfLabels
+        let stepValue = minStepValue.multipleOf(Constants.XStepValueShouldBeMultipleOf)
+        
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.NoStyle
+        formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        let seconds = NSCalendar.currentCalendar().component(.CalendarUnitSecond, fromDate: minX)
+        
+        for var xValue: CGFloat = 60.0 - CGFloat(seconds) ; xValue < CGFloat(timeInterval) ; xValue += stepValue {
+            let x = xValue / CGFloat(timeInterval) * bounds.width
+            let location = CGPoint(x: x, y: bounds.height - Constants.XScaleMarginBottom)
+            let date = minX.dateByAddingTimeInterval(NSTimeInterval(xValue))
+            drawText(formatter.stringFromDate(date), toPoint: location)
+        }
+    }
+    
+    
+    func drawAxesInRect(bounds: CGRect, minX: NSDate, maxX: NSDate, minY: CGFloat = 0, maxY: CGFloat) {
+
+        drawX(bounds, minX: minX, maxX: maxX)
+        drawY(bounds, minY: minY, maxY: maxY)
+    }
+    
+}
+
+private extension CGFloat {
+    func multipleOf(val: CGFloat) -> CGFloat {
+        return ceil(self / val) * val
+    }
+}
--- SmartMeter/GraphView.swift
+++ SmartMeter/GraphView.swift
@@ -17,91 +17,17 @@ protocol GraphViewDatasource {
 class GraphView: UIView {
     
     private struct Constants {
-        static let LabelHorizontalSpacing: CGFloat = 120
-        static let LabelVerticalSpacing: CGFloat = 60
-        static let XStepValueShouldBeMultipleOf: CGFloat = 60 // seconds
-        static let YStepValueShouldBeMultipleOf: CGFloat = 100
-        static let XScaleMarginBottom: CGFloat = 42
-        static let YScaleMarginLeft: CGFloat = 8
-        static let LegendColor: UIColor = UIColor.lightGrayColor()
+        static let lineWidth:CGFloat = 1.5
         static let NumSamplesPerPixelRatio: CGFloat = 1.0 // 1.0 will fully use the retina resolution
         static let NumSamplesPerPixelRatioInPanOrZoomMode: CGFloat = 0.25
     }
     
-    
     var datasource: GraphViewDatasource?
+    
+    private let axesDrawer = AxesDrawer()
 
     var maxY: CGFloat {
         return CGFloat(datasource!.graphViewGetMaxY())
-    }
-    
-    var minX: NSDate? {
-        if let firstSamle =  datasource?.graphViewgetSample(0, resample: 1) {
-            return firstSamle.timestamp
-        }
-        return nil
-    }
-
-    var maxX: NSDate? {
-        if let count =  datasource?.graphViewgetSampleCount() {
-            return minX?.dateByAddingTimeInterval(NSTimeInterval(count))
-        }
-        return nil
-    }
-
-    struct DrawConstants {
-        static let lineWidth:CGFloat = 1.5
-    }
-    
-    private func drawLegends() {
-
-        let attributes = [
-            NSFontAttributeName : UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote),
-            NSForegroundColorAttributeName : Constants.LegendColor
-        ]
-
-        // X Legend
-        if let timeInterval = maxX?.timeIntervalSinceDate(minX!) {
-            let maximumNumberOfLabels = bounds.width / Constants.LabelHorizontalSpacing
-            let minStepValue = CGFloat(timeInterval) / maximumNumberOfLabels
-            let stepValue = minStepValue.multipleOf(Constants.XStepValueShouldBeMultipleOf)
-            
-            let formatter = NSDateFormatter()
-            formatter.dateStyle = NSDateFormatterStyle.NoStyle
-            formatter.timeStyle = NSDateFormatterStyle.ShortStyle
-            
-            let seconds = NSCalendar.currentCalendar().component(.CalendarUnitSecond, fromDate: minX!)
-            
-            for var xValue: CGFloat = 60.0 - CGFloat(seconds) ; xValue < CGFloat(timeInterval) ; xValue += stepValue {
-                let x = xValue / CGFloat(timeInterval) * bounds.width
-                let location = CGPoint(x: x, y: bounds.height - Constants.XScaleMarginBottom)
-                
-                let date = minX?.dateByAddingTimeInterval(NSTimeInterval(xValue))
-                let label = formatter.stringFromDate(date!)
-                var textRect = CGRect(origin: location, size: label.sizeWithAttributes(attributes))
-                label.drawInRect(textRect, withAttributes: attributes)
-            }
-            
-        }
-
-        // Y Legend
-        let maximumNumberOfLabels = bounds.height / Constants.LabelVerticalSpacing
-        let minStepValue = maxY / maximumNumberOfLabels
-        let stepValue = minStepValue.multipleOf(Constants.YStepValueShouldBeMultipleOf)
-        
-        let formatter = NSNumberFormatter()
-        formatter.maximumFractionDigits = 1
-        formatter.minimumIntegerDigits = 1
-        
-        for var yValue: CGFloat = stepValue ; yValue < maxY ; yValue += stepValue {
-            let y = bounds.height - (yValue / maxY * bounds.height)
-            let location = CGPoint(x: Constants.YScaleMarginLeft, y: y)
-            if let label = formatter.stringFromNumber(yValue)?.stringByAppendingString("W") {
-                var textRect = CGRect(origin: location, size: label.sizeWithAttributes(attributes))
-                label.drawInRect(textRect, withAttributes: attributes)
-            }
-        }
-    
     }
     
     override func drawRect(rect: CGRect) {
@@ -135,14 +61,12 @@ class GraphView: UIView {
             }
         }
         UIColor.blackColor().set()
-        path.lineWidth = DrawConstants.lineWidth
+        path.lineWidth = Constants.lineWidth
         path.stroke()
-        drawLegends()
-    }
-}
-
-extension CGFloat {
-    func multipleOf(val: CGFloat) -> CGFloat {
-        return ceil(self / val) * val
+        if let minX = datasource?.graphViewgetSample(0, resample: 1)?.timestamp,
+            let maxX = datasource?.graphViewgetSample(datasource!.graphViewgetSampleCount()-1, resample: 1)?.timestamp
+        {
+            axesDrawer.drawAxesInRect(bounds, minX: minX, maxX: maxX, minY: 0, maxY: maxY)
+        }
     }
 }
